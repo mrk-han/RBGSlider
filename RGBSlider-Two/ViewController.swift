dbaa3b0c39da48fb2a161f857d26e947e1d79eea
mrk-han <mark.han@willowtreeapps.com>
2022-02-18 18:45:16 -0500

    initial commit of rgbslider

---
+++ RGBSlider-Two/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  RGBSlider-Two
+//
+//  Created by Mark Han on 2/18/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var colorView: UIView!
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.colorView.layer.borderWidth = 8
+        self.colorView.layer.borderColor = UIColor.white.cgColor
+        self.colorView.layer.masksToBounds = true
+        self.colorView.clipsToBounds = true
+        self.colorView.layer.cornerRadius = 5
+        
+        self.redSlider.setValue(0.5, animated: true)
+        self.greenSlider.setValue(0.5, animated: true)
+        self.blueSlider.setValue(0.5, animated: true)
+        
+        changeBoxColor()
+    }
+    
+    @IBAction func changeBoxColor() {
+        
+        
+        
+        let r: CGFloat = CGFloat(self.redSlider.value)
+        let g: CGFloat = CGFloat(self.greenSlider.value)
+        let b: CGFloat = CGFloat(self.blueSlider.value)
+        
+        colorView.backgroundColor = UIColor(red: r, green: g, blue: b, alpha: 1)
+    }
+
+}
+
