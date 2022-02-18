c3c1b414a481752feb141038c8c51ce9ab6a1dad
mrk-han <mark.han@willowtreeapps.com>
2022-02-18 18:50:40 -0500

    code cleanup

--- RGBSlider-Two/ViewController.swift
+++ RGBSlider-Two/ViewController.swift
@@ -14,10 +14,9 @@ class ViewController: UIViewController {
     @IBOutlet var greenSlider: UISlider!
     @IBOutlet var blueSlider: UISlider!
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
         self.colorView.layer.borderWidth = 8
         self.colorView.layer.borderColor = UIColor.white.cgColor
         self.colorView.layer.masksToBounds = true
@@ -32,15 +31,10 @@ class ViewController: UIViewController {
     }
     
     @IBAction func changeBoxColor() {
-        
-        
-        
         let r: CGFloat = CGFloat(self.redSlider.value)
         let g: CGFloat = CGFloat(self.greenSlider.value)
         let b: CGFloat = CGFloat(self.blueSlider.value)
         
         colorView.backgroundColor = UIColor(red: r, green: g, blue: b, alpha: 1)
     }
-
 }
-
